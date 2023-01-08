21e82f838c2276ab1ce851c3aa6e4c802e954829
FELYPE RAMIREZ ALVES <113054755+FelypeRA@users.noreply.github.com>
2023-01-08 15:16:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ def tela():
 
     print('\n   -----------------')
 
+
 def dadosjogadores():
   jogador1.append(str(input("Qual o nome do jogador 1: ")))
-  jogador1.append(str(input('Deseja jogar com X ou O: ')))
+  jogador1.append(str(input('Deseja jogar com X ou O: ')).upper())
   jogador2.append(str(input("Qual o nome do jogador 2: ")))
   if jogador1[1] == 'X':
     jogador2.append(str('O'))
@@ -55,42 +56,73 @@ while True:
     jogo[linha][coluna] = jogador2[1]
   os.system("clear")
   tela()
+
+  
+#verificando vitoria em cada linha
   for line in jogo:
-    c = 0 
+    contX = contO = 0
     for item in line:
       if item == 'X':
-        c += 1
-        if c == 3:         
+        contX += 1
+        if contX == 3:         
           if jogador1[1] == 'X':
             vencedor = 'Jogador 1'
+            break
           else: 
-            vencedor = 'Jogador 2  '
-          break
+            vencedor = 'Jogador 2'
+            break
       elif item == 'O':
-        c += 1
-        if c == 3:
+        contO += 1
+        if contO == 3:
           if jogador2[1] == 'O':
-            vencedor = 'Jogador 2 TEST'
+            vencedor = 'Jogador 2'
+            break
           else: 
             vencedor = 'Jogador 1'
-          break
-      if vencedor != '':
-        break
-  if vencedor != '':
-     break
+            break
+    if vencedor != '':
+      break
+
+# Verificando vitoria nas diagonais 
   if jogo[0][0] == jogo[1][1] == jogo[2][2] and jogo[0][0] != '':
-    if jogador1[1] == 'X' or jogador1[1] == 'O':
+    if jogador1[1] == jogo[1][1]:
       vencedor = 'Jogador 1'
       break
     else: 
       vencedor = 'Jogador 2'
       break
   if jogo[0][2] == jogo[1][1] == jogo[2][0] and jogo[0][2] != '':
-    if jogador1[1] == 'X' or jogador1[1] == 'O':
+    if jogador1[1] == jogo[1][1]:
       vencedor = 'Jogador 1'
       break
     else: 
       vencedor = 'Jogador 2'
       break
+
+# verificando vitorias nas colunas 
+  for i in range(0,3):
+    contadorX = contadorO = 0
+    for elemento in jogo:
+      if elemento[i] == "X":
+        contadorX += 1
+      if contadorX == 3: 
+        if jogador1[1] == 'X':
+          vencedor = 'Jogador 1'
+          break
+        else: 
+          vencedor = 'Jogador 2'
+          break
+        
+      if elemento[i] == "O":
+        contadorO += 1
+      if contadorO == 3:
+        if jogador1[1] == 'O':
+          vencedor = 'Jogador 1'
+          break
+        else: 
+          vencedor = 'Jogador 2'
+          break
+  if vencedor != '':
+    break
   cont += 1
 print(f'{vencedor} venceu a partida!')
